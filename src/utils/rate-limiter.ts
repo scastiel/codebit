f29485f12d9236b8b7d367b0d50e24345ac65337
Maxime David <maxime.david@datadoghq.com>
2023-11-02 01:01:46 -0400

    remove yarn lock

--- src/utils/rate-limiter.ts
+++ src/utils/rate-limiter.ts
@@ -1,5 +1,6 @@
 import { env } from '@/lib/env'
 import LRUCache from 'lru-cache'
+
 export default function rateLimiter() {
   const tokenCache = new LRUCache({
     max: env.RATE_LIMIT_MAX_ITEMS_IN_CACHE,
@@ -9,16 +10,14 @@ export default function rateLimiter() {
   return {
     check: (limit: number, token: string) =>
       new Promise<Headers>((resolve, reject) => {
-        console.log('lets check')
         const tokenCount = (tokenCache.get(token) as number[]) || [0]
-        console.log(tokenCount)
         if (tokenCount[0] === 0) {
           tokenCache.set(token, tokenCount)
         }
         tokenCount[0] += 1
         const currentUsage = tokenCount[0]
         const isRateLimited = currentUsage >= limit
-        console.log('isRateLimited', isRateLimited)
+
         const rateLimitHeaders = new Headers()
         rateLimitHeaders.set('X-RateLimit-Limit', limit.toString())
         rateLimitHeaders.set(
